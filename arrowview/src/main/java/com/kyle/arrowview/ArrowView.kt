cad88100761e37a99b3099d743567dcbf8b2852b
zhengxu@zmit.cn <zidanshen55>
2019-10-24 15:31:09 +0800

    1.0.3

--- arrowview/src/main/java/com/kyle/arrowview/ArrowView.kt
+++ arrowview/src/main/java/com/kyle/arrowview/ArrowView.kt
@@ -19,12 +19,22 @@ class ArrowView : View {
     //箭头方向
     private var mDirection = Direction.LEFT
 
+    private var DEFAULT_LINE_COLOR = Color.parseColor("#606060")
+    private var mLineColor = DEFAULT_LINE_COLOR
+
 
     enum class Direction {
         TOP, LEFT, RIGHT, BOTTOM
     }
 
     constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ArrowView)
+        mLineColor = typedArray.getColor(R.styleable.ArrowView_av_lineColor, DEFAULT_LINE_COLOR)
+        mDirection= Direction.values()[typedArray.getInt(R.styleable.ArrowView_av_direction,Direction.LEFT.ordinal)]
         init()
     }
 
@@ -36,13 +46,10 @@ class ArrowView : View {
         mLineRadius = Math.toRadians(90.0)
 
         mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-        mPaint.color = Color.parseColor("#606060")
+        mPaint.color = mLineColor
         mPaint.strokeWidth = DensityUtil.dip2px(context, 2f).toFloat()
     }
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init()
-    }
 
     class Builder(view: ArrowView) {
         var arrowView = view
@@ -84,7 +91,7 @@ class ArrowView : View {
         super.onDraw(canvas)
         Log.e("onDraw", "onDraw")
         val points: ArrayList<Float> = ArrayList()
-        var headPointX: Float
+        val headPointX: Float
         var headPointY = 0.0f
         val halfRadius = mLineRadius / 2
         val sinHalfRadius = Math.sin(halfRadius)
@@ -147,21 +154,21 @@ class ArrowView : View {
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        var heightSpec=heightMeasureSpec
-        var widthSpec=widthMeasureSpec
-        val overX = (Math.sin(mLineRadius/2) * mPaint.strokeWidth / 2).toFloat()
-        val overY = (Math.cos(mLineRadius/2) * mPaint.strokeWidth / 2).toFloat()
+        var heightSpec = heightMeasureSpec
+        var widthSpec = widthMeasureSpec
+        val overX = (Math.sin(mLineRadius / 2) * mPaint.strokeWidth / 2).toFloat()
+        val overY = (Math.cos(mLineRadius / 2) * mPaint.strokeWidth / 2).toFloat()
         if (MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.AT_MOST) {
             var height = 0f
             height = when (mDirection) {
                 Direction.TOP, Direction.BOTTOM -> {
-                    (Math.cos(mLineRadius / 2) * mLineLength).toFloat()+2*overY
+                    (Math.cos(mLineRadius / 2) * mLineLength).toFloat() + 2 * overY
                 }
                 Direction.LEFT, Direction.RIGHT -> {
                     2 * (Math.sin(mLineRadius / 2) * mLineLength).toFloat()
                 }
             }
-            heightSpec=MeasureSpec.makeMeasureSpec(height.toInt(),MeasureSpec.EXACTLY)
+            heightSpec = MeasureSpec.makeMeasureSpec(height.toInt(), MeasureSpec.EXACTLY)
         }
         if (MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.AT_MOST) {
             var width = 0f
@@ -170,10 +177,10 @@ class ArrowView : View {
                     2 * (Math.sin(mLineRadius / 2) * mLineLength).toFloat()
                 }
                 Direction.LEFT, Direction.RIGHT -> {
-                    (Math.cos(mLineRadius / 2) * mLineLength).toFloat()+2*overX
+                    (Math.cos(mLineRadius / 2) * mLineLength).toFloat() + 2 * overX
                 }
             }
-            widthSpec=MeasureSpec.makeMeasureSpec(width.toInt(),MeasureSpec.EXACTLY)
+            widthSpec = MeasureSpec.makeMeasureSpec(width.toInt(), MeasureSpec.EXACTLY)
         }
         super.onMeasure(widthSpec, heightSpec)
     }
